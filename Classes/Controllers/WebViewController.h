c85ed34459bc5f28636e52167aa7fdf2dec7305a
Peter Shih <ptshih@gmail.com>
2010-11-10 15:37:44 -0800

    Adding webviewcontroller for FB profile

---
+++ Classes/Controllers/WebViewController.h
@@ -0,0 +1,39 @@
+//
+//  WebViewController.h
+//  Facemash
+//
+//  Created by Peter Shih on 10/14/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//#define kFixedItemWidthPortrait 60
+//#define kFixedItemWidthLandscape 140
+
+@interface WebViewController : UIViewController <UIWebViewDelegate, UIActionSheetDelegate> {
+	IBOutlet UIWebView *_myWebView;
+	IBOutlet UIToolbar *_myToolbar;
+	IBOutlet UINavigationItem *_navBarItem;
+	UIBarButtonItem *_backButton;
+	UIBarButtonItem *_forwardButton;
+	UIBarButtonItem *_stopButton;
+	UIBarButtonItem *_refreshButton;
+	UIActivityIndicatorView *_activityView;
+	BOOL _isRefreshState;
+}
+@property (nonatomic, retain) UIBarButtonItem *backButton;
+@property (nonatomic, retain) UIBarButtonItem *forwardButton;
+@property (nonatomic, retain) UIBarButtonItem *stopButton;
+@property (nonatomic, retain) UIBarButtonItem *refreshButton;
+@property (nonatomic, retain) UIActivityIndicatorView *activityView;
+
+//- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil withURL:(NSString*)urlString;
+- (void)loadURL:(NSString*)urlString;
+- (void)loadURLRequest:(NSURLRequest*)urlRequest;
+- (void)moreActions;
+- (void)setNewsTitle:(NSString*)newsTitle;
+
+- (IBAction)dismissView;
+
+@end
---
+++ Classes/Controllers/WebViewController.m
@@ -0,0 +1,183 @@
+//
+//  WebViewController.m
+//  Facemash
+//
+//  Created by Peter Shih on 10/14/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "WebViewController.h"
+#import "Constants.h"
+
+@interface WebViewController (Private)
+
+- (void)setupToolbar;
+
+@end
+
+@implementation WebViewController
+
+@synthesize backButton = _backButton;
+@synthesize forwardButton = _forwardButton;
+@synthesize stopButton = _stopButton;
+@synthesize refreshButton = _refreshButton;
+@synthesize activityView = _activityView;
+
+// The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil /*withURL:(NSString*)urlString*/ {
+	if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+		// Custom initialization
+		_isRefreshState = NO;
+	}
+	return self;
+}
+
+- (void)setNewsTitle:(NSString*)newsTitle {
+	[_navBarItem setTitle:newsTitle];
+}
+
+- (void)loadURL:(NSString*)urlString {
+	NSURLRequest *req = [NSURLRequest requestWithURL:[NSURL URLWithString:urlString]];
+	[_myWebView loadRequest:req];
+}
+
+- (void)loadURLRequest:(NSURLRequest*)urlRequest {
+	[_myWebView loadRequest:urlRequest];
+}
+
+- (IBAction)dismissView {
+	if(_myWebView.loading) {
+		[_myWebView stopLoading];
+	}
+	[self dismissModalViewControllerAnimated:YES];
+}
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+	[super viewDidLoad];
+  [self setupToolbar];
+}
+
+- (void)setupToolbar {
+  self.activityView = [[[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite] autorelease];
+	UIBarButtonItem *actButton = [[UIBarButtonItem alloc] initWithCustomView:self.activityView];
+	_navBarItem.rightBarButtonItem = actButton;
+	[actButton release];
+	
+	self.backButton = [[[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:WEBVIEW_LEFT] style:UIBarButtonItemStylePlain target:_myWebView action:@selector(goBack)] autorelease];
+	UIBarButtonItem *flexibleItem1 = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+	self.forwardButton = [[[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:WEBVIEW_RIGHT] style:UIBarButtonItemStylePlain target:_myWebView action:@selector(goForward)] autorelease];
+	UIBarButtonItem *flexibleItem2 = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+	self.stopButton = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemStop target:_myWebView action:@selector(stopLoading)] autorelease];
+	UIBarButtonItem *flexibleItem3 = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];	
+	UIBarButtonItem *actionButton = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(moreActions)] autorelease];
+  
+	self.refreshButton = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:_myWebView action:@selector(reload)] autorelease];
+	
+	NSArray *bararray = [[NSArray alloc] initWithObjects:self.backButton, flexibleItem1, self.forwardButton, flexibleItem2, self.stopButton, flexibleItem3, actionButton, nil];
+	_myToolbar.items = bararray;
+	
+	[bararray release];
+	[flexibleItem1 release];
+	[flexibleItem2 release];
+	[flexibleItem3 release];
+}
+
+
+#pragma mark -
+#pragma mark UIWebViewDelegate Functions
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+	[self.activityView startAnimating];
+	self.backButton.enabled = [_myWebView canGoBack];
+	self.forwardButton.enabled = [_myWebView canGoForward];
+	
+	NSMutableArray *array = [[NSMutableArray alloc] initWithArray:_myToolbar.items];
+	[array replaceObjectAtIndex:4 withObject:self.stopButton];
+	_myToolbar.items = array;
+	[array release];
+	//stopButton.enabled = YES;
+}
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+//	AppDelegate_Phone *appDelegate = [[UIApplication sharedApplication] delegate];
+//	[appDelegate hideNetworkActivity];
+	[self.activityView stopAnimating];
+	self.backButton.enabled = [_myWebView canGoBack];
+	self.forwardButton.enabled = [_myWebView canGoForward];
+	
+	NSMutableArray *array = [[NSMutableArray alloc] initWithArray:_myToolbar.items];
+	[array replaceObjectAtIndex:4 withObject:self.refreshButton];
+	_myToolbar.items = array;
+	[array release];
+	//stopButton.enabled = NO;
+}
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+//	AppDelegate_Phone *appDelegate = [[UIApplication sharedApplication] delegate];
+//	[appDelegate hideNetworkActivity];
+	[self.activityView stopAnimating];
+	self.backButton.enabled = [_myWebView canGoBack];
+	self.forwardButton.enabled = [_myWebView canGoForward];
+
+	NSMutableArray *array = [[NSMutableArray alloc] initWithArray:_myToolbar.items];
+	[array replaceObjectAtIndex:4 withObject:self.refreshButton];
+	_myToolbar.items = array;
+	[array release];
+	//stopButton.enabled = NO;	
+}
+
+#pragma mark -
+#pragma mark Action Button Functions
+
+- (void)moreActions {
+//	DealWatchAppDelegate *delegate = [[UIApplication sharedApplication] delegate];
+	UIActionSheet *styleAlert =[[[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil
+																							otherButtonTitles:@"Open in Safari", @"Copy link", nil] autorelease];
+	styleAlert.actionSheetStyle = UIActionSheetStyleBlackOpaque;
+	styleAlert.destructiveButtonIndex = 4;
+	[styleAlert showInView:self.view];
+}
+
+- (void)actionSheet:(UIActionSheet *)modalView clickedButtonAtIndex:(NSInteger)buttonIndex
+{	
+  switch (buttonIndex) {
+		// opening URL in Safari
+    case 0: [[UIApplication sharedApplication] openURL:[[_myWebView request] URL]]; break;
+		// copy URL to clipboard
+    case 1: {
+			NSString *urlString = [[_myWebView.request URL] absoluteString];
+			UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+			[pasteboard setString:urlString];
+			break;
+		}
+    default: break;	// case 2 is cancel anyway
+  }
+}
+
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+  if(UIInterfaceOrientationIsLandscape(interfaceOrientation)) return YES;
+  else return NO;
+}
+
+- (void)didReceiveMemoryWarning {
+	// Releases the view if it doesn't have a superview.
+  [super didReceiveMemoryWarning];
+	
+	// Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload {
+	// Release any retained subviews of the main view.
+	// e.g. self.myOutlet = nil;
+}
+
+- (void)dealloc {
+	[_backButton release];
+	[_forwardButton release];
+	[_stopButton release];
+	[_refreshButton release];
+	[_activityView release];
+	[super dealloc];
+}
+
+@end
